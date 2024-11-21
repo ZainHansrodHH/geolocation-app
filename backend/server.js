c579e591d8269078c884070ae2ad596233a168a3
Zain Hansrod <zainhansrod786@gmail.com>
2024-11-21 16:00:47 -0500

    Modified the server file to accomidate the specific tables were looking to add based on our CSV file

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,39 @@
 //Create a connection to our prosgresSQL server and migrate the CSV content into it. 
 
+const { Client } = require('pg');
 const fs = require('fs');
 const csv = require('csv-parser');
-const { Pool } = require('pg');
-require('dotenv').config();
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
 
 const migrateCSVToDatabase = () => {
-    fs.createReadStream('./data/locations.csv')
+
+    const connectionString = `postgresql://u_f3147c00_57b5_42fa_bd26_adb1d4f7a580:7Mi22Awm8bOn97W405GCo7Qc48nWO2JPU7oZ2v7FbDi261Vss5WZ@pg.rapidapp.io:5433/db_f3147c00_57b5_42fa_bd26_adb1d4f7a580?ssl=true&sslmode=no-verify&application_name=rapidapp_nodejs`;
+    const client = new Client({
+        connectionString,
+    })
+    
+    client.connect().then((rsp) => {
+        console.log("Successfully connected to the database!");
+        fs.createReadStream('./data/locations.csv')
         .pipe(csv())
         .on('data', async (row) => {
-            const { name, latitude, longitude } = row;
-            await pool.query(
-                'INSERT INTO locations (name, latitude, longitude) VALUES ($1, $2, $3)',
-                [name, latitude, longitude]
+            const { street, city, zip_code, county, country, latitude, longitude, time_zone } = row;
+
+            await client.query(
+                `INSERT INTO locations (street, city, zip_code, county, country, latitude, longitude, time_zone) 
+                 VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
+                [street, city, zip_code, county, country, latitude, longitude, time_zone]
             );
         })
         .on('end', () => {
             console.log('CSV migration completed.');
         });
+    }).catch((err) => {
+        console.log(err)
+    });
+
+
 };
 
 migrateCSVToDatabase();
